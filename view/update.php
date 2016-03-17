4d11030dd7b770c04b198cfe32e2cfef15b178a5
John Espitia <john.espitia@johnksoft.com.co>
2016-03-17 02:11:33 -0500

    @johnksft - Capa de vistas de la aplicación

---
+++ view/update.php
@@ -0,0 +1,12 @@
+<?php
+
+$controller = new Controller();
+if ($controller->update($_POST['x'], $_POST['y'], $_POST['z'], $_POST['w'])) {
+    $value = $controller->getCube();
+    echo json_encode(array(
+        "result" => "ok",
+        "message" => "Update ejecutado",
+        "return" => $value[$_POST['x']][$_POST['y']][$_POST['z']]
+    ));
+}
+?>
