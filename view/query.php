49b5d5f169a3e2ccc12c445a348983b2bc55c447
John Espitia <john.espitia@johnksoft.com.co>
2016-03-18 08:54:10 -0500

    @johnksft - Capa de vistas de la aplicación fase 2

--- view/query.php
+++ view/query.php
@@ -1,3 +1,9 @@
 <?php
-
+$controller = new Controller();
+$result=$controller->query($_POST['x1'], $_POST['y1'], $_POST['z1'], $_POST['x2'], $_POST['y2'], $_POST['z2']);
+echo json_encode(array(
+        "result" => "ok",
+        "message" => "Query ejecutado",
+        "return" => $result
+    ));
 ?>
